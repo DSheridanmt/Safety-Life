b71abf68de290135e0ce01c25939c1f1cd1901a7
Álvaro José <93918945+Arkhanthus@users.noreply.github.com>
2021-12-20 09:54:55 -0300

    Implementando rotas corrigido

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import HomeView
+from .views import CadastroView, ExerciciosView, HomeView, LoginView, LogoutView, PerfilView, ReceitasView, SobreView
 urlpatterns = [ 
-    path('', HomeView.as_view(), name= 'home')
+    path('', HomeView.as_view(), name= 'home'),
+    path('login/', LoginView.as_view(), name = 'login'),
+    path('perfil/', PerfilView.as_view(), name= 'perfil'),
+    path('cadastro/', CadastroView.as_view(), name= 'cadastro'),
+    path('receitas/', ReceitasView.as_view(), name= 'receitas'),
+    path('exercicios/', ExerciciosView.as_view(), name= 'exercicios'),
+    path('logout/', LogoutView.as_view(),name= 'logout'),
+    path('sobre/', SobreView.as_view(), name= 'sobre'),
     
+
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -6,3 +6,24 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+class LoginView(TemplateView):
+    template_name  = 'login.html'
+
+class ReceitasView(TemplateView):
+    template_name = 'receitas.html'
+
+class ExerciciosView(TemplateView):
+    template_name ='exercicios.html'
+
+class PerfilView(TemplateView):
+    template_name ='perfil.html'
+
+class SobreView(TemplateView):
+    template_name ='sobre.html'
+
+class CadastroView(TemplateView):
+    template_name ='cadastro.html'
+
+class LogoutView(TemplateView):
+    template_name ='logout.html'
