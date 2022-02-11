afa862c3c6270bfc9023b2397ee9241d73474407
Álvaro José <alvaro.j@escolar.ifrn.edu.br>
2022-02-10 21:51:26 -0300

    correção

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import CadastroView, ExerciciosView, HomeView, LoginView, PerfilView, ReceitasView
+from .views import CadastroView, HomeView, LoginView, PerfilView, PublicaçõesView
 urlpatterns = [ 
     path('', HomeView.as_view(), name= 'home'),
     path('login/', LoginView.as_view(), name = 'login'),
     path('perfil/', PerfilView.as_view(), name= 'perfil'),
     path('cadastro/', CadastroView.as_view(), name= 'cadastro'),
-    path('receitas/', ReceitasView.as_view(), name= 'receitas'),
-    path('exercicios/', ExerciciosView.as_view(), name= 'exercicios'),
+    path('publicaçoes/', PublicaçõesView.as_view(), name= 'publicaçoes'),
+   
     
     
     
