6367ef4001d97128268e1f5dea967858cfde4b7f
SafetyLife <78332195+DSheridanmt@users.noreply.github.com>
2022-03-09 21:59:46 -0300

    Implementando publicações - parte 1

--- paginas/views.py
+++ paginas/views.py
@@ -1,17 +1,17 @@
 # Create your views here.
 
 from django.views.generic import TemplateView
-
+from django.shortcuts import render
 from .models import Publicacao
 
 class HomeView(TemplateView):
     template_name = 'index.html'
 
-class LoginView(TemplateView):
-    template_name  = 'login.html'
-
 class PublicacoesView(TemplateView):
     template_name = 'publicacoes.html'
+
+class SobreView(TemplateView):
+    template_name = 'sobre.html'
 
 
 
