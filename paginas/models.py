e8e8113edca62dcb09416d09dc5c46ee0166112c
SafetyLife <78332195+DSheridanmt@users.noreply.github.com>
2022-03-15 15:45:51 -0300

    publicações - parte 4

--- paginas/models.py
+++ paginas/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from distutils.command.upload import upload
 from turtle import window_height
 from types import ClassMethodDescriptorType
@@ -7,22 +8,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import Field
 from django.db.models.fields.related import ForeignKey
+from django.forms import MultipleChoiceField
 from django.utils import timezone
 
 # Create your models here.
 
 class Tag(models.Model):
-    nomeTag = models.CharField(max_length= 20, verbose_name='Tag')
+    nomeTag = models.CharField(max_length= 40, verbose_name='Tag')
 
     def __str__(self):
         return self.nomeTag
 
 class Publicacao(models.Model):
-    titulo = models.CharField(max_length=50, verbose_name='Título')
-    descricao = models.TextField(max_length=200, verbose_name='Descrição')
-    hora = models.DateField(auto_now_add=True)
-    imagem = models.ImageField(upload_to='arquivos')
-    upload = models.FileField(upload_to='arquivos')
+    titulo = models.CharField(max_length=50, verbose_name='Título', editable=True)
+    descricao = models.TextField(max_length=200, verbose_name='Descrição', editable=True)
+    hora = models.DateTimeField(auto_now_add=True)
+    imagem = models.ImageField(upload_to='arquivos', blank=True, verbose_name='Imagem', editable=True)
+    upload = models.FileField(upload_to='arquivos', blank=True, verbose_name='Arquivo', editable=True)
+    link = models.URLField(max_length=200, blank=True, verbose_name='Link', editable=True)
     tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
 
     def __str__(self):
